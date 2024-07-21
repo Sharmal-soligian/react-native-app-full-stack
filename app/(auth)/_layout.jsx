c0271c870ed05f5c9f38269195c0d2f4b954db11
Sharmal-soligian <soligiansharmal@gmail.com>
2024-07-21 20:42:27 +0530

    first commit

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+const AuthLayout = () => {
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      </Stack>
+
+      {/* STATUS BAR */}
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
+  );
+};
+
+export default AuthLayout;
